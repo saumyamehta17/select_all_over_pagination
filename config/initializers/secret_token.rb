369fe814c7e5cba10d08bd4b7718965e44ff5be6
sweetymehta <ashish.saini@aequor.com>
2014-06-10 01:26:18 +0530

    selected all checkbox over pagination pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaginationSelectAll::Application.config.secret_key_base = '7fe351cf3d5c6109cae21ea9dff4301004db8bd7fd680f24769adb2e0ad7a563bab70729f43a88a4875a7c65e25898a082195bc757a00d23013aeaa508d3024a'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+PaginationSelectAll::Application.routes.draw do
+  resources :products
+  root 'products#index'
+end
