e29fdc55810ac349ac5aa8648d438f994251a869
sweetymehta <ashish.saini@aequor.com>
2014-06-10 01:51:44 +0530

    selected all checkbox over pagination pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+20.times {|x| Product.create(name: "Product#{x}", description: "My product#{x}", price: "#{x}")}
